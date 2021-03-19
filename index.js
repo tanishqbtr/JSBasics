b3d407f8ab37fa33edbc782f377c8df95e39961f
tanishqbtr <tanishqbtr@gmail.com>
2021-03-19 15:43:37 +0530

    Some new JS commands

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ console.log('Hello World!');
 
 // Making changes and trying out git commit thorugh terminal
 console.log('Hello World for a second time!');
+
+let name = 'Hello';
+
+console.log(name);
+
+let interestRate = 0.3;
+interestRate = 1;
+
+console.log(interestRate);
+
+console.log(typeof name);
+
+let person = {
+    name : 'Tanishq',
+    age: 30
+
+};
+
+console.log(person);
+
+person[name] = 'Not Tanishq';
+
+console.log(person);
+
+person.name= 'Tanishq Again!'
+
+console.log(person);
